0dee8902aeada0f68fa03f8eb242d84f5aa98960
FuyangShan <40450679+FuyangShan@users.noreply.github.com>
2020-01-25 09:18:58 -0800

    First Commit

---
+++ app/src/main/java/com/laioffer/matrix/OnBoardingActivity.java
@@ -0,0 +1,14 @@
+package com.laioffer.matrix;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OnBoardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
+    }
+}
