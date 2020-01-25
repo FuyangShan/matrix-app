39421b9979209e84233a8f221a24a597be0da876
FuyangShan <40450679+FuyangShan@users.noreply.github.com>
2020-01-25 10:03:48 -0800

    First Fragment for Login

--- app/src/main/java/com/laioffer/matrix/OnBoardingActivity.java
+++ app/src/main/java/com/laioffer/matrix/OnBoardingActivity.java
@@ -3,12 +3,26 @@ package com.laioffer.matrix;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class OnBoardingActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_boarding);
+
+        viewPager = findViewById(R.id.viewpager);
+
+        OnBoardingPageAdapter onBoardingPageAdapter = new OnBoardingPageAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(onBoardingPageAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
